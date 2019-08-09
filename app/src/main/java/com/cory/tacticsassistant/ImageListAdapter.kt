b4bd1ad82761ff64d6338268c9da6ebed684594a
Cory <625283654@qq.com>
2019-08-09 19:10:22 +0800

    update

---
+++ app/src/main/java/com/cory/tacticsassistant/ImageListAdapter.kt
@@ -0,0 +1,24 @@
+package com.cory.tacticsassistant
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+}
+
+class ImageListAdapter : RecyclerView.Adapter<ImageHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ImageHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
